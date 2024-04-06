3b628821c9b009c1a16c3fe1364e9a7468b7c805
heynleee <152494291+heynleee@users.noreply.github.com>
2024-04-06 16:09:18 +0900

    Update 과제 안 내신 분．．？.java

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -20,6 +20,28 @@ public class Main {
         }
     }
 
+/*방법 2
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] a = new int[31];
+
+        for(int i=1; i<=28; i++) {
+            a[scanner.nextInt()]++;
+        }
+        for(int j=1; j<=30; j++) {
+            if(a[j] == 0) {
+                System.out.println(j);
+            }
+        }
+        }
+    }
+*/
+
+
 
 
 
